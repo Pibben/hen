2bca15c0a415221fa4a752377e90a124dad1b8b0
Per Zetterlund <per@pz.se>
2014-10-08 13:54:45 +0200

    Timer.

--- utils.h
+++ utils.h
@@ -42,6 +42,25 @@ inline Eigen::Matrix4f proj(float left, float right,
     return m;
 }
 
+template <int enable>
+struct Timer {
+    void report(const std::string& msg) {}
+};
+
+template <>
+struct Timer<1> {
+    decltype(std::chrono::steady_clock::now()) latest;
+    Timer() {
+        latest = std::chrono::steady_clock::now();
+    }
+    void report(const std::string& msg = "") {
+        auto now = std::chrono::steady_clock::now();
+        auto diff = now - latest;
+
+        std::cout << msg << ": " << std::chrono::duration <double, std::micro> (diff).count() << " us" << std::endl;
+        latest = now;
+    }
+};
 
 
 #endif /* UTILS_H_ */
