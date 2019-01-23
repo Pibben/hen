e5fd1df7bd4c3c2ac0d67ec003e27a8e89926ce8
Per Zetterlund <per@pz.se>
2019-01-23 21:20:06 +0100

    PNG loader

--- io.h
+++ io.h
@@ -16,7 +16,10 @@
 
 #include <boost/algorithm/string/predicate.hpp>
 
+#include "png.h"
+
 #include "veclib.h"
+#include "utils.h"
 
 inline VecLib::Vector3f parseVertex(const std::string& str) {
     VecLib::Vector3f v;
@@ -144,6 +147,137 @@ inline void loadObj(const std::string& filename, std::vector<VecLib::Vector3f>* 
             std::cout << "Unknown line: " << line << std::endl;
         }
     }
+}
+
+class FileWrapper {
+public:
+    FileWrapper(const std::string& filename) {
+        mFile = fopen(filename.c_str(), "rb");
+    }
+    ~FileWrapper() {
+        if(mFile) {
+            fclose(mFile);
+        }
+    }
+    FileWrapper(const FileWrapper&) = delete;
+    FileWrapper(FileWrapper&&) = delete;
+    FileWrapper& operator=(const FileWrapper&) = delete;
+    FileWrapper& operator=(FileWrapper&&) = delete;
+
+    size_t read(char* ptr, size_t size) {
+        return fread(ptr, 1, size, mFile);
+    }
+
+    FILE* getFile() {
+        return mFile;
+    }
+
+    operator bool() {
+        return mFile != nullptr;
+    }
+private:
+    FILE* mFile{nullptr};
+};
+
+inline PixelBuffer<uint8_t> loadPng(const std::string& filename) {
+    FileWrapper ifs(filename);
+    assert(ifs);
+    std::vector<unsigned char> ingress(8);
+    ifs.read((char*)ingress.data(), 8);
+    assert(!png_sig_cmp(ingress.data(), 0, 8));
+
+    png_voidp user_error_ptr = 0;
+    png_error_ptr user_error_fn = 0;
+    png_error_ptr user_warning_fn = 0;
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, user_error_ptr, user_error_fn, user_warning_fn);
+    assert(png_ptr);
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    assert(info_ptr);
+
+    png_infop end_info = png_create_info_struct(png_ptr);
+    assert(end_info);
+
+    const bool ok = !setjmp(png_jmpbuf(png_ptr));
+    assert(ok);
+
+    png_init_io(png_ptr, ifs.getFile());
+    png_set_sig_bytes(png_ptr, 8);
+
+    png_read_info(png_ptr, info_ptr);
+    png_uint_32 W, H;
+    int bit_depth, color_type, interlace_type;
+    bool is_gray = false;
+    png_get_IHDR(png_ptr, info_ptr, &W, &H, &bit_depth, &color_type, &interlace_type, nullptr, nullptr);
+
+    printf("%d x %d @ %d\n", W, H, bit_depth);
+
+    if (color_type == PNG_COLOR_TYPE_PALETTE) {
+        png_set_palette_to_rgb(png_ptr);
+        color_type = PNG_COLOR_TYPE_RGB;
+        bit_depth = 8;
+    } else if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) {
+        png_set_expand_gray_1_2_4_to_8(png_ptr);
+        is_gray = true;
+        bit_depth = 8;
+    }
+
+    if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) {
+        png_set_tRNS_to_alpha(png_ptr);
+        color_type |= PNG_COLOR_MASK_ALPHA;
+    }
+
+    if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA) {
+        png_set_gray_to_rgb(png_ptr);
+        color_type |= PNG_COLOR_MASK_COLOR;
+        is_gray = true;
+    }
+
+    if (color_type == PNG_COLOR_TYPE_RGB) {
+        png_set_filler(png_ptr, 0xffffU, PNG_FILLER_AFTER);
+    }
+
+    png_read_update_info(png_ptr, info_ptr);
+
+    assert(bit_depth == 8);
+
+    const int byte_depth = bit_depth >> 3;
+
+    png_bytep* const imgData = new png_bytep[H];
+    for (unsigned int row = 0; row < H; ++row) {
+        imgData[row] = new png_byte[(size_t)byte_depth * 4 * W];
+    }
+    png_read_image(png_ptr, imgData);
+    png_read_end(png_ptr, end_info);
+
+    assert(color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_RGB_ALPHA);
+
+    const bool is_alpha = (color_type == PNG_COLOR_TYPE_RGBA);
+
+    assert(!is_gray);
+    assert(is_alpha);
+
+    PixelBuffer<uint8_t> pb(W, H, byte_depth * 4);
+
+    for (uint_fast16_t y = 0; y < H; ++y) {
+        const unsigned char *ptrs = (unsigned char*)imgData[y];
+        for (uint_fast16_t x = 0; x < W; ++x) {
+            pb(x, y, 0) = *ptrs++;
+            pb(x, y, 1) = *ptrs++;
+            pb(x, y, 2) = *ptrs++;
+            pb(x, y, 3) = *ptrs++;
+        }
+    }
+
+    png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+
+    for (uint_fast16_t y = 0; y < H; ++y) {
+        delete[] imgData[y];
+    }
+
+    delete[] imgData;
+
+    return std::move(pb);
 }
 
 #endif /* IO_H_ */
